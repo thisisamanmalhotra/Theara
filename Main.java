8c8fe61558eaaf6f4a65f6a10d107971a99f2995
akarsh-shrivastava <akarsh.mails@rediffmail.com>
2019-02-26 19:03:09 +0530

    completed cli

--- Main.java
+++ Main.java
@@ -6,10 +6,11 @@ import java.awt.event.*;
 class Window
 {
     JFrame f;
-    JLabel cli;
+    JLabel[] cli;
     JLabel face;
     JLabel chat;
-    JScrollPane scroll;
+    String cliText;
+    String cliBuff;
 
     KeyListener k;
 
@@ -22,48 +23,151 @@ class Window
         createChat();
         createFace();
         createCli();
+        addKeyListener();
         f.setVisible(true);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     void createCli()
     {
-        cli = new JLabel("kjsgvjeklbgvjle");
-        cli.setBounds(337,522,953,168);
-        cli.setFont(new Font(Font.MONOSPACED, cli.getFont().getStyle(), 17));
-        cli.setOpaque(true);
-        cli.setBackground(new Color(0,0,0));
-        cli.setForeground(new Color(0,200,0));
-        //cli.setLineWrap(true);
+        cli = new JLabel[8];
+        for (int i=0; i<8; i++)
+        {
+            cli[i] = new JLabel("");
+            cli[i].setBounds(337,530+(20*i),953,20);
+            cli[i].setFont(new Font(Font.MONOSPACED, cli[i].getFont().getStyle(), 17));
+            cli[i].setOpaque(true);
+            cli[i].setBackground(new Color(0,0,0));
+            cli[i].setForeground(new Color(0,200,0));
+        }
+        cliText = "";
+        cliBuff = ">>> ";
+        for (int i=0; i<8; i++)
+            f.add(cli[i]);
+        cli[7].setText(">>> ");
+    }
 
-        scroll = new JScrollPane (cli);
-        scroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-
+    void addKeyListener()
+    {
         k = new KeyListener()
         {
             @Override
             public void keyTyped(KeyEvent key)
             {
-                System.out.println("Key typed code=" + key.getKeyCode() + ", char=" + key.getKeyChar());
+                //System.out.println("Key typed code=" + key.getKeyCode() + ", char=" + key.getKeyChar());
+                char temp = key.getKeyChar();
+                editCli(temp);
             }
 
             @Override
             public void keyPressed(KeyEvent key)
             {
-                System.out.println("Key pressed code=" + key.getKeyCode() + ", char=" + key.getKeyChar());
+                //System.out.println("Key pressed code=" + key.getKeyCode() + ", char=" + key.getKeyChar());
             }
 
             @Override
             public void keyReleased(KeyEvent key)
             {
-                System.out.println("Key released code=" + key.getKeyCode() + ", char=" + key.getKeyChar());
+                //System.out.println("Key released code=" + key.getKeyCode() + ", char=" + key.getKeyChar());
             }
         };
 
-
         f.addKeyListener(k);
-        f.add(cli);
-        f.add(scroll);
+    }
+
+    void editCli(char temp)
+    {
+        if((temp>=(char)32&&temp<=(char)126))
+            cliBuff += temp;
+        else if(temp==8)
+        {    
+            try
+            {
+                cliBuff = cliBuff.substring(0, cliBuff.length()-1);
+            }
+            catch (java.lang.StringIndexOutOfBoundsException ex){}
+        }
+        else if (temp == '\n') 
+        {
+            if(cliBuff.length()<85)
+                cliText += (cliBuff + '\n');
+            else
+            {
+                cliText += (cliBuff.substring(0,85) + "\n");
+                cliText += (cliBuff.substring(85, cliBuff.length()) + "\n");
+            }
+            callBash();
+            cliBuff = ">>> ";
+        }
+        setCliText();
+    }
+
+    void setCliText()
+    {
+        int m=0,n=0;
+        String[] cliLines = cliText.split("\n", 0);
+        m = cliLines.length;
+        String cli2ndLastLine;
+        String cliLastLine;
+        if (cliBuff.length()>85)
+        {
+            cliLastLine    = cliBuff.substring(85, cliBuff.length());
+            cli2ndLastLine = cliBuff.substring(0,85);
+            n=2;
+        }
+        else
+        {
+            cliLastLine    = cliBuff;
+            cli2ndLastLine = "";
+            n=1;
+        }
+        if (m+n>8)
+        {
+            if (n==1)
+            {
+                cli[7].setText(cliBuff);
+                for(int i=0;i<7;i++)
+                {
+                    cli[i].setText(cliLines[m-7+i]);
+                }
+            }
+            else if(n==2)
+            {
+                cli[7].setText(cliLastLine);
+                cli[6].setText(cli2ndLastLine);
+                for(int i=0;i<6;i++)
+                {
+                    cli[i].setText(cliLines[m-6+i]);
+                }
+            }
+        }
+        else
+        {
+            if (n==1)
+            {
+                cli[7].setText(cliBuff);
+                for(int i=0;i<m;i++)
+                {
+                    cli[7-m+i].setText(cliLines[i]);
+                }
+            }
+            else if(n==2)
+            {
+                cli[7].setText(cliLastLine);
+                cli[6].setText(cli2ndLastLine);
+                for(int i=0;i<m;i++)
+                {
+                    cli[6-m+i].setText(cliLines[i]);
+                }
+            }
+
+        }
+            
+    }
+
+    void callBash()
+    {
+        System.out.println(cliBuff + " executed");
     }
 
     void createChat()
@@ -82,7 +186,7 @@ class Window
     void createFace()
     {
         face = new JLabel();
-        face.setBounds(337,10,953,502);
+        face.setBounds(337,10,953,510);
         Font f1 = face.getFont();
         Font f2 = new Font(f1.getFontName(), f1.getStyle(), f1.getSize()+5);
         face.setFont(f2);
